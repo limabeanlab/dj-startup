5de03a233f3bee2905a38a65e2dfc8f08ab99b51
limabean <limabean@lbl.local>
2024-03-18 14:09:08 -0700

    added DEBUG

--- dj_startup/settings.py
+++ dj_startup/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv("DEBUG", default='False') == 'True'
-DEBUG = True
+DEBUG = os.getenv("DEBUG", default='False')
 
 ALLOWED_HOSTS = []
 
